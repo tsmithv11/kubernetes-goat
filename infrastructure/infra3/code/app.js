f7d5c005601168deb41802346e272a84f75eb9ff
Taylor <tsmith.v11@gmail.com>
2023-08-06 22:03:51 -0700

    Update app.js

--- infrastructure/infra3/code/app.js
+++ infrastructure/infra3/code/app.js
@@ -4,19 +4,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { spawnSync} = require('child_process');
 
-const func123 = express();
-func123.use(bodyParser.urlencoded({
+const func123func = express();
+func123func.use(bodyParser.urlencoded({
     extended: true
 }));
-func123.use(bodyParser.json());
-func123.use(express.json());
-func123.use(express.static("express"));
+func123func.use(bodyParser.json());
+func123func.use(express.json());
+func123func.use(express.static("express"));
 
-func123.get('/', function (req, res) {
+func123func.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-func123.post('/', function (req, res) {
+func123func.post('/', function (req, res) {
     var endpoint = req.body.endpoint, method = req.body.method || 'GET', headers = req.body.headers || {};
 
     const child = spawnSync('curl', [endpoint, '-H', headers, '-X', method]);
@@ -30,7 +30,7 @@ func123.post('/', function (req, res) {
     }
 })
 
-const server = http.createServer(func123);
+const server = http.createServer(func123func);
 const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
