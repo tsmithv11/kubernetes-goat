b5537d3c67e4de548a990ca9e6343bc693ae4ca5
Taylor <tsmith.v11@gmail.com>
2023-08-06 22:11:44 -0700

    Update app.js

--- infrastructure/infra3/code/app.js
+++ infrastructure/infra3/code/app.js
@@ -4,15 +4,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { spawnSync} = require('child_process');
 
-const func123 = express();
-func123.use(bodyParser.urlencoded({
+const func1234 = express();
+func1234.use(bodyParser.urlencoded({
     extended: true
 }));
-func123.use(bodyParser.json());
-func123.use(express.json());
-func123.use(express.static("express"));
+func1234.use(bodyParser.json());
+func1234.use(express.json());
+func1234.use(express.static("express"));
 
-func123.post('/', function (req, res) {
+func1234.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+func1234.post('/', function (req, res) {
     var endpoint = req.body.endpoint, method = req.body.method || 'GET', headers = req.body.headers || {};
 
     const child = spawnSync('curl', [endpoint, '-H', headers, '-X', method]);
@@ -26,11 +30,7 @@ func123.post('/', function (req, res) {
     }
 })
 
-func123.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-const server = http.createServer(func123);
+const server = http.createServer(func1234);
 const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
